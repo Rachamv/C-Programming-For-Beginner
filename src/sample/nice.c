8e812a755be183b29def38cf84c58d9caa91bac8
Adewunmi Oladele <87828937+Rachamv@users.noreply.github.com>
2022-10-24 13:32:20 +0100

    Add files via upload

---
+++ src/sample/nice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Addition of numbers
+ * Return: Always 0 (success).
+ */
+
+int main() {
+int a = 45;
+int b = 5;
+int c = a + b;
+printf("%d", c);
+return 0;
+}
